f35436c83e89e6b256cdc206547b564d1c016e5d
Marko Bader <bader.marko@gmail.com>
2025-01-29 18:43:57 +0100

    Reverting back file (unintended commit)

--- web/wp-content/themes/starter-2024/theme/preflight.php
+++ web/wp-content/themes/starter-2024/theme/preflight.php
@@ -4,6 +4,6 @@ if ( ! file_exists( __DIR__ . '/../vendor/autoload.php' ) ) :
 	wp_die( 'You forgot to run composer install' );
 endif;
 
-//if ( ! function_exists( 'get_field' ) ) :
-//  wp_die( 'You need to install and activate Advanced Custom Fields Pro' );
-//endif;
+if ( ! function_exists( 'get_field' ) ) :
+	wp_die( 'You need to install and activate Advanced Custom Fields Pro' );
+endif;
