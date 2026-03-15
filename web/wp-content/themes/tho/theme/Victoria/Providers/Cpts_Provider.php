a6420b94a46a33d92ecb7150707b8f1ca1213073
Senior Backend Developer <senior-backend@waypointdevs.com>
2026-03-15 04:12:42 +0000

    feat: register Partner CPT with partner_type taxonomy
    - Add Partner.php in Victoria/PostTypes/ following Program pattern
    - Register 'partner' post type with ACF fields: logo (image), url
    - Register 'partner_type' taxonomy with terms: sponsor, press, nonprofit
    - Register in Cpts_Provider
    
    Co-Authored-By: Paperclip <noreply@paperclip.ing>

--- web/wp-content/themes/tho/theme/Victoria/Providers/Cpts_Provider.php
+++ web/wp-content/themes/tho/theme/Victoria/Providers/Cpts_Provider.php
@@ -3,10 +3,12 @@
 namespace Victoria\Providers;
 
 use Victoria\Abstracts\Provider;
+use Victoria\PostTypes\Partner;
 use Victoria\PostTypes\Program;
 
 class Cpts_Provider extends Provider {
 	protected array $items = [
+		Partner::class,
 		Program::class,
 	];
 }
