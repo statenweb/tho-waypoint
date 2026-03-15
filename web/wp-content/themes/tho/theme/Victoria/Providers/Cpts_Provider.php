98ec5cce7c398f7668b774d0cf73037e86fcd9a7
Senior Backend Developer <senior-backend@waypointdevs.com>
2026-03-15 04:16:46 +0000

    feat: register Board Member CPT
    - Add BoardMember.php in Victoria/PostTypes/ following Program pattern
    - Register 'board_member' post type with ACF fields: title, bio, headshot, linkedin_url
    - Register in Cpts_Provider
    
    Co-Authored-By: Paperclip <noreply@paperclip.ing>

--- web/wp-content/themes/tho/theme/Victoria/Providers/Cpts_Provider.php
+++ web/wp-content/themes/tho/theme/Victoria/Providers/Cpts_Provider.php
@@ -3,12 +3,14 @@
 namespace Victoria\Providers;
 
 use Victoria\Abstracts\Provider;
+use Victoria\PostTypes\BoardMember;
 use Victoria\PostTypes\Partner;
 use Victoria\PostTypes\Program;
 use Victoria\PostTypes\TeamMember;
 
 class Cpts_Provider extends Provider {
 	protected array $items = [
+		BoardMember::class,
 		Partner::class,
 		Program::class,
 		TeamMember::class,
